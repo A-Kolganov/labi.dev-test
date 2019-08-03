372bb0e3861d16f02705af6101a9818982a3b2f8
Artem Kolganov <tema.crow@gmail.com>
2019-08-03 12:41:20 +0300

    testing

--- src/js/components/viewList.js
+++ src/js/components/viewList.js
@@ -1,5 +1,5 @@
 import templ from '../../templates/list-template.hbs';
-
+import {showTxt} from '../view/showTxt';
 
 function displayItems(e){
     const appendMain = document.querySelector('.search-list__list');
@@ -12,13 +12,31 @@ function displayItems(e){
     }).then( data =>{
         
         data.posts.forEach( item=>{
-            console.log(item.client.rating);
             
-            appendMain.insertAdjacentHTML("afterbegin", templ(item))
-            const rating = document.querySelector('.gold')
-            rating.setAttribute("style", "width:"+Number(item.client.rating)*20+'%' )
-             
-            console.log()
+            
+            appendMain.insertAdjacentHTML("afterbegin", templ(item));
+            const rating = document.querySelector('.search-item__rating-cover__gold');
+            rating.setAttribute("style", "width:"+Number(item.client.rating)*20+'%' );
+            
+            const txtBox = document.querySelector('.search-item__title__txt');
+    
+
+
+            if(item.description.length > 350){
+                txtBox.textContent = item.description.slice(0,300) + '...';
+                const txtElse = item.description.slice(300);
+                console.log(item.description.slice(300));
+                txtBox.insertAdjacentHTML('beforeend', '<a href="#" class=" search-item__title__txt__btn" >more</a>')
+                const showMoreBtn = document.querySelector('.search-item__title__txt__btn');
+                showMoreBtn.addEventListener( 'click', showTxt);
+                txtBox.insertAdjacentHTML('afterend', '<p class="search-item__title__txt__hidden" style="display: none">'+txtElse+'</p>')
+
+            } else{
+                txtBox.textContent = item.description;
+            }
+            
+            
+            
         })
 
     }).catch(err=> console.log(err))
